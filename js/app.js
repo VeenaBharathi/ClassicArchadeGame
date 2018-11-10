04701520c7bab773966b6cbed9bbb506c156ff52
Veena Bharathi Akilla <veenabharathi93@gmail.com>
2018-11-10 21:30:41 +0530

    Space corrections

--- js/app.js
+++ js/app.js
@@ -1,41 +1,37 @@
 // Enemies our player must avoid
 var allEnemies = [];
 
+// Enemy class
 var Enemy = function(x, y, speed) {
-
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
     this.x = x;
     this.y = y;
     this.speed = speed;
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
-    this.x += Math.round((this.speed)*dt);
-    
-    if(this.x>550){
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x += Math.round((this.speed)*dt);  
+    if(this.x>550) {
         this.x = -100;
         let index = allEnemies.indexOf(this);
         allEnemies.splice(index,1);
         pre_y = [0], y1 = 0, 
         setEnemies(setYAxis(), setSpeed());     
-    }
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    }  
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 
@@ -43,58 +39,56 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Player = function(x, y){
- this.x = x;
- this.y = y;
- this.girl = 'images/char-cat-girl.png';
+// Player class
+var Player = function(x, y) {
+     this.x = x;
+     this.y = y;
+     this.girl = 'images/char-cat-girl.png';
 }
 
-Player.prototype.update = function(dt){};
+Player.prototype.update = function(dt) {
+
+};
 
 Player.prototype.render = function() {
     // if(this.x>=0 && this.x<=400 && this.y <=400 && this.y>=-20) 
      ctx.drawImage(Resources.get(this.girl), this.x, this.y);
 
- if(this.x < 0) {
-    this.x = 400;
- }
- else if(this.x > 400) {
-    this.x = 0;
- }
- else if(this.y < -20){
-    this.y = 400
- }
-  else if(this.y > 400){
-    this.y = 400;
- }
+     if(this.x < 0) {
+        this.x = 400;
+     }
+     else if(this.x > 400) {
+        this.x = 0;
+     }
+     else if(this.y < -20){
+        this.y = 400;
+     }
+      else if(this.y > 400){
+        this.y = 400;
+     }
 
+    //collision logic 
     let currX = this.x;
     let currY = this.y;
-    // console.log(currX + " " + currY);
     allEnemies.forEach(function(enemy){
-    // console.log("enemy " + enemy.x + " " + enemy.y );
-    // console.log(enemy.x + "=" + (currX+40) + " or " + enemy.x + "=" + (currX-40)  + 
-    // " and " + enemy.y +"=" + (currY+40) + " or " + enemy.y + "=" + (currY-40) + ".");
-    
-    if((enemy.x <= (currX+40) && enemy.x >= (currX-40)) && (enemy.y <= (currY+40) && enemy.y >= (currY-40)))
-       {
-           currX = 200;
-           currY = 400;
+        if((enemy.x <= (currX+40) && enemy.x >= (currX-40)) && (enemy.y <= (currY+40) && enemy.y >= (currY-40))) {
+               currX = 200;
+               currY = 400;
         }
     });
-
+    
+    // set to normal position when collided
     this.x = currX;
     this.y = currY;
-};
+}
 
-Player.prototype.handleInput = function(code){
-
-    switch(code){
-        // 37: 'left',38: 'up', 39: 'right', 40: 'down'
-
+Player.prototype.handleInput = function(code) {
+  // change position based on key presses
+  // 37: 'left',38: 'up', 39: 'right', 40: 'down'
+    switch(code) {
         case "left": 
             this.x-=100;
-              if(this.x <= 0)
+            if(this.x <= 0)
                 this.x = 0;
             break;
         case "up": 
@@ -102,56 +96,46 @@ Player.prototype.handleInput = function(code){
             break;
         case "right": 
             this.x+=100;
-              if(this.x >= 400)
+            if(this.x >= 400)
                 this.x = 400;
             break;
         case "down": 
             this.y+=60;
-             if(this.y >= 400)
+            if(this.y >= 400)
                 this.y = 400;
             break;
-       
     }
 
-    
-    if(this.y ===  -20){
+    // Game over when player reaches top
+    if(this.y ===  -20) {
        gameOver();
     }
+}
 
-};
+// changing y position of enemies randomly
+var pre_y = [0], y1 = 0, flag;
 
-
-// Now instantiate your objects.
-
-
-//-100 to 550
-
-var pre_y = [0], y1 = 0, flag ;
-
-function setYAxis(){
+function setYAxis() {
    y1 = Math.round((Math.random() * (300 - 60)) + 60);
-
-    pre_y.forEach(function(pre){
-    if( Math.abs(y1 - pre) < 50){
-     
-        flag =true;
-        return null;
+   pre_y.forEach(function(pre) {
+       if( Math.abs(y1 - pre) < 50) {
+            flag =true;
+            return null;
         }
     });
-
-    if(flag != true){
+    if(flag != true) {
         pre_y.push(y1);  
         return y1;
     }
 }
 
-function setSpeed(){
+// set random speed to enemies
+function setSpeed() {
     return (Math.random()*(400 - 60)) + 60;
 }
 
-
-
-for(var i = 0; i<4; i++){
+// create 4 enemies with random speeds and positions
+for(var i = 0; i<4; i++) {
     flag = false;
     var yaxis = setYAxis();
     var sp = setSpeed();
@@ -163,17 +147,14 @@ for(var i = 0; i<4; i++){
     }
 }
 
- 
- 
+// Now instantiate your objects. 
 function setEnemies(yaxis, sp){
         var enemy = new Enemy(-100,yaxis,sp);
+        // Place all enemy objects in an array called allEnemies
         allEnemies.push(enemy);
 }
 
-// Place all enemy objects in an array called allEnemies
-
 // Place the player object in a variable called player
-
 var player = new Player(200,400);
 
 // This listens for key presses and sends the keys to your
@@ -189,12 +170,10 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
-
-
-
-function gameOver(){
+// Game over alert thrown when player reaches top. Game restarts.
+function gameOver() {
          setTimeout(alert("game over!!! you won!!"), 1000);
-        if(confirm){
+        if(confirm) {
             window.location.reload(true);
         }
 }
